6842b6cb31dd1223c031251f5f3ee1af41ea547f
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-12-01 22:06:40 -0500

    Add LocationBar, for real

---
+++ src/LocationBar.js
@@ -0,0 +1,34 @@
+import { React, useState, useEffect } from 'react';
+import './App.css';
+import Alert from 'react-bootstrap/Alert';
+
+function LocationBar() {
+  const [location, setLocation] = useState([null,null])
+  const [showLocation, setShowLocation] = useState(true);
+
+
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation([position.coords.latitude, position.coords.longitude])
+    })
+  }
+
+  useEffect(() => {
+    getLocation()
+  }, []);
+
+  if (showLocation) {
+    return (
+      <div className="App">
+        <Alert variant="info" onClose={() => setShowLocation(false)} dismissible>
+          Current location: { (location[0] && location[1] ) ? location.join(',') : "Location not set"}
+        </Alert>
+
+      </div>
+    );
+  } else {
+    return null
+  }
+}
+
+export default LocationBar;
