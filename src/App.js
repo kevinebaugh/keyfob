70743b9e2c58771aae244d91fb362486706f06e3
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-12-06 09:52:04 -0500

    Handle start/stop event

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ function App() {
   function handleStartStop(event) {
     console.log("handleStartStop", handleStartStop)
     console.log(event.target.id)
+
+    const eventInfo = {
+      type: event.target.id,
+      time: Date.now(),
+      location: null
+    }
+
+    fetch("http://localhost:3001/starts-stops", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(eventInfo),
+    })
+      .then((r) => r.json())
+      .then((response) => console.log(response))
   }
 
   return (
