ee9a9871ffc71a353bd9271601e84ce31cd5e3a9
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-12-08 22:41:28 -0500

    Add delete button

--- src/Tools.js
+++ src/Tools.js
@@ -1,13 +1,26 @@
-import { React } from 'react';
+import { React, useEffect } from 'react';
 import './App.css';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 
-function Tools() {
+function Tools( {startStopEvents} ) {
+
+  function deleteAllEvents() {
+    startStopEvents.map((event) => {
+      fetch(`http://localhost:4000/starts-stops/${event.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => console.log(`Deleted ${event.id}`));
+    })
+  }
+
   return (
     <div className="Tools">
       <h1>Tools</h1>
-      <h2>Some tools here</h2>
+      <ButtonGroup vertical>
+        <Button onClick={deleteAllEvents} variant="outline-danger">{`⚠️ Delete all ${startStopEvents.length} events`}</Button>
+      </ButtonGroup>
     </div>
   )
 }
